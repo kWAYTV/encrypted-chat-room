ac2beb336ef2cdaaf38ebbaf534bbb745620548c
kWAY <55437151+kWAYTV@users.noreply.github.com>
2023-11-09 07:40:02 +0100

    ➕Reworked the server file to make it as it should be done from the beginning.

--- server.py
+++ server.py
@@ -1,101 +1,96 @@
-import sys, socket, threading
+import socket, sys, threading
 from threading import Lock
 from kwslogger import Logger
 
-# Initialize the logger
-logger = Logger()
+class Client:
+    def __init__(self):
+        self.logger = Logger()
+        # lock for handling multiple clients
+        self.lock = Lock()
+        # An IPv4 address for the server with port.
+        self.server, self.port = '127.0.0.1', 8000
+        # Address is stored as a tuple
+        self.address = (self.server, self.port)
+        # the format in which encoding and decoding will occur
+        self.encoding_format = "utf-8"
+        # Lists that will contains all the clients/rooms connected to the server.
+        self.rooms = {'CHAT': {}, 'PLUTO': {}}
+        # Create a new socket for the server
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # connect to the server
+        self.client.bind(self.address)
+        # Create a logo for the server
+        self.logger.create_logo("EC Server")
 
-logger.create_logo("EC Server")
+    def start_chat(self):
+        self.logger.info(f"Server started on {self.server}:{self.port}")
 
-# An IPv4 address for the server with port.
-SERVER, PORT = '127.0.0.1', 8000
+        # listening for connections
+        self.client.listen()
 
-# Address is stored as a tuple
-ADDRESS = (SERVER, PORT)
+        while True:
+            # accept connections and returns a new connection to the client and the address bound to it
+            conn, addr = self.client.accept()
 
-# the format in which encoding and decoding will occur
-FORMAT = "utf-8"
+            # receive room name
+            room = conn.recv(64).decode()
+            if room in self.rooms:  # if room exists send acknowledgment
+                conn.send(b'ok')
 
-# Lists that will contains all the clients/rooms connected to the server.
-rooms = {'CHAT': {}, 'PLUTO': {}}
+                # append the client to the respective list
+                self.rooms[room][addr[0] + str(addr[1])] = conn
 
-# Create a new socket for the server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                name = conn.recv(64).decode()
+                conn.send(b'ok')  # send acknowledgment
+                joined = conn.recv(4096)
+                conn.send(b'ok')  # send acknowledgment
+                left = conn.recv(4096)
+                conn.send(b'ok')  # send acknowledgment
 
-# bind the address of the server to the socket
-server.bind(ADDRESS)
+                # Start the handling thread
+                thread = threading.Thread(target=self.handle, args=(conn, addr, room, name, joined, left))
+                thread.start()
+            
+            else:
+                self.logger.warning("Room does not exist")
+                conn.close()
 
-# lock for handling multiple clients
-lock = Lock()
+    def handle(self, conn, addr, room, name, joined, left):
+        with self.lock:
+            self.logger.info(f"Active connections {threading.active_count() - 1}.")
+            self.logger.info(f"New connection {addr} = {name} to {room}.")
+            self.broadcast_message(joined, room)
 
-# function to start the connection
-def start_chat():
-    logger.info(f"Server started on {SERVER}:{PORT}")
+            while True:
+                try:
+                    # receive message
+                    message = conn.recv(4096)
+                    # broadcast message
+                    self.broadcast_message(message, room)
+                except:
+                    break
 
-    # listening for connections
-    server.listen()
-
-    while True:
-        # accept connections and returns a new connection to the client and the address bound to it
-        conn, addr = server.accept()
-
-        # receive room name
-        room = conn.recv(64).decode()
-        if room in rooms:  # if room exists send acknowledgment
-            conn.send(b'ok')
-
-            # append the client to the respective list
-            rooms[room][addr[0] + str(addr[1])] = conn
-
-            name = conn.recv(64).decode()
-            conn.send(b'ok')  # send acknowledgment
-            joined = conn.recv(4096)
-            conn.send(b'ok')  # send acknowledgment
-            left = conn.recv(4096)
-            conn.send(b'ok')  # send acknowledgment
-
-            # Start the handling thread
-            thread = threading.Thread(target=handle, args=(conn, addr, room, name, joined, left))
-            thread.start()
-
-        else:
-            logger.warning('Room does not exist.')
+            # close the connection
             conn.close()
 
-# incoming messages
-def handle(conn, addr, room, name, joined, left):
-    with lock:
-        logger.info(f"Active connections {threading.active_count() - 1}.")
-        logger.info(f"New connection {addr} = {name} to {room}.")
-        broadcast_message(joined, room)
-        while True:
-            try:
-                # receive message
-                message = conn.recv(4096)
-                # broadcast message
-                broadcast_message(message, room)
-            except:
-                break
+            # remove the client from the clients list
+            del self.rooms[room][addr[0] + str(addr[1])]
+            self.logger.info(f"The user {name} has disconnected.")
+            self.broadcast_message(left, room)
 
-        # close the connection
-        conn.close()
+    def broadcast_message(self, message, room):
+        for addr in self.rooms[room]:
+            self.rooms[room].get(addr).send(message)
 
-        # remove the client from the clients list
-        del rooms[room][addr[0] + str(addr[1])]
-        logger.info(f"The user {name} has disconnected.")
-        broadcast_message(left, room)
-
-# method for broadcasting messages to the each clients
-def broadcast_message(message, room):
-    for addr in rooms[room]:
-        rooms[room].get(addr).send(message)
-
-# begin the communication
-try:
-    start_chat()
-except KeyboardInterrupt:
-    server.close()
-    sys.exit(0)
-except:
-    server.close()
-    sys.exit(0)
+if __name__ == '__main__':
+    client = Client()
+    try:
+        client.start_chat()
+    except KeyboardInterrupt:
+        client.logger.info("Goodbye!")
+        client.client.close()
+        sys.exit(1)
+    except Exception as e:
+        client.logger.error(f"An error occured {e}.")
+        client.client.close()
+        sys.exit(1)
